4ea5a0d0d81230dd571b1891d3c90db86ad542ed
Alek30k <alemank23k@gmail.com>
2024-08-30 16:24:03 -0300

    last commit

---
+++ client/src/layouts/rootLayout/rootLayout.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const RootLayout = () => {
+  return (
+    <div className="">
+      <header>
+        <Link to="/"></Link>
+      </header>
+    </div>
+  );
+};
+
+export default RootLayout;
