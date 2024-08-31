045f2235b2e33106cc12db67beecb83c17eda938
Alek30k <alemank23k@gmail.com>
2024-08-31 15:51:36 -0300

    last commit

--- client/src/layouts/rootLayout/rootLayout.jsx
+++ client/src/layouts/rootLayout/rootLayout.jsx
@@ -1,10 +1,11 @@
 import { Link, Outlet } from "react-router-dom";
+import "./rooLayout.css";
 
 const RootLayout = () => {
   return (
-    <div className="">
+    <div className="rootLayout">
       <header>
-        <Link to="/">
+        <Link to="/" className="logo">
           <img src="/logo.png" alt="logo" />
           <span>ALE IA</span>
         </Link>
--- client/src/routes/dashboardPage/DashboardPage.jsx
+++ client/src/routes/dashboardPage/DashboardPage.jsx
@@ -1,7 +1,15 @@
+import { Outlet } from "react-router-dom";
 import "./dashboardPage.css";
 
 const DashboardPage = () => {
-  return <div className="dashboardpage">DashboardPage</div>;
+  return (
+    <div className="dashboardpage">
+      <div className="menu">Menu</div>
+      <div className="content">
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
