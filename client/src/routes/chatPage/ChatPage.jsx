ba14c2a29114263d2cfa392790349b74e291b19c
Alek30k <alemank23k@gmail.com>
2024-09-10 22:24:21 -0300

    last commit

--- client/src/routes/chatPage/ChatPage.jsx
+++ client/src/routes/chatPage/ChatPage.jsx
@@ -1,7 +1,76 @@
 import "./chatPage.css";
+// import NewPrompt from "../../components/newPrompt/NewPrompt";
+// import { useQuery } from "@tanstack/react-query";
+// import { useLocation } from "react-router-dom";
+// import Markdown from "react-markdown";
+// import { IKImage } from "imagekitio-react";
 
 const ChatPage = () => {
-  return <div className="chatpage">ChatPage</div>;
+  // const path = useLocation().pathname;
+  // const chatId = path.split("/").pop();
+
+  // const { isPending, error, data } = useQuery({
+  //   queryKey: ["chat", chatId],
+  //   queryFn: () =>
+  //     fetch(`${import.meta.env.VITE_API_URL}/api/chats/${chatId}`, {
+  //       credentials: "include",
+  //     }).then((res) => res.json()),
+  // });
+
+  // console.log(data);
+
+  return (
+    <div className="chatPage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>{" "}
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>{" "}
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>{" "}
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>{" "}
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>{" "}
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>{" "}
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>{" "}
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>{" "}
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>{" "}
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>
+          <div className="message">message from ia</div>
+          <div className="message user">message from user</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatPage;
