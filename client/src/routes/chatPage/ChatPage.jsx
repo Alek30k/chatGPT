bd466753a0a8b6b7d068ca38cc606e932d30172c
Alek30k <alemank23k@gmail.com>
2024-09-11 22:44:28 -0300

    last commit

--- client/src/routes/chatPage/ChatPage.jsx
+++ client/src/routes/chatPage/ChatPage.jsx
@@ -1,26 +1,8 @@
 import NewPrompt from "../../components/newPrompt/NewPrompt";
 import "./chatPage.css";
 import { useEffect, useRef } from "react";
-// import NewPrompt from "../../components/newPrompt/NewPrompt";
-// import { useQuery } from "@tanstack/react-query";
-// import { useLocation } from "react-router-dom";
-// import Markdown from "react-markdown";
-// import { IKImage } from "imagekitio-react";
 
 const ChatPage = () => {
-  // const path = useLocation().pathname;
-  // const chatId = path.split("/").pop();
-
-  // const { isPending, error, data } = useQuery({
-  //   queryKey: ["chat", chatId],
-  //   queryFn: () =>
-  //     fetch(`${import.meta.env.VITE_API_URL}/api/chats/${chatId}`, {
-  //       credentials: "include",
-  //     }).then((res) => res.json()),
-  // });
-
-  // console.log(data);
-
   const endRef = useRef(null);
 
   useEffect(() => {
