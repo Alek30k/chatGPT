bf489316bc8723b5369ae8f6e6123182b640371a
Alek30k <alemank23k@gmail.com>
2024-09-12 22:57:14 -0300

    last commit

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log("avshdvavsdbvas");
+import express from "express";
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+
+app.listen(port, () => {
+  connect();
+  console.log("Server running on 3000");
+});
