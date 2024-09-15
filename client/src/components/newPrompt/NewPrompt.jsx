49a65ede92917a1b9bed298b91a6afd1cff0cf7d
Alek30k <alemank23k@gmail.com>
2024-09-14 23:25:43 -0300

    last commit

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -24,9 +24,7 @@ const NewPrompt = () => {
       <div className="newPrompt">
         <form className="newForm">
           <Upload setImg={setImg} />
-          <label htmlFor="file">
-            <img src="/attachment.png" alt="" />
-          </label>
+
           <input id="file" type="file" hidden />
           <input type="text" name="text" placeholder="Ask anything..." />
           <button>
