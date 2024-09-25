e492059b411980d2886d8212282f8c7fe8286ebf
Alek30k <alemank23k@gmail.com>
2024-09-25 16:41:19 -0300

    last commit

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import ImageKit from "imagekit";
 import cors from "cors";
 import mongoose from "mongoose";
 import Chat from "./models/chat.js";
+import UserChats from "./models/userChats.js";
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -37,63 +38,54 @@ app.get("/api/upload", (req, res) => {
 });
 
 app.post("/api/chats", async (req, res) => {
-  const userId = req.auth.userId;
-  const { text } = req.body;
+  // const userId = req.auth.userId;
+  const { userId, text } = req.body;
 
   try {
+    //CREATE A NEW CHAT
     const newChat = new Chat({
       userId: userId,
       history: [{ role: "user", parts: [{ text }] }],
     });
+
+    const savedChat = await newChat.save();
+
+    //   // CHECK IF THE USERCHATS EXISTS
+    const userChats = await UserChats.find({ userId: userId });
+
+    //   // IF DOESN'T EXIST CREATE A NEW ONE AND ADD THE CHAT IN THE CHATS ARRAY
+    if (!userChats.length) {
+      const newUserChats = new UserChats({
+        userId: userId,
+        chats: [
+          {
+            _id: savedChat._id,
+            title: text.substring(0, 40),
+          },
+        ],
+      });
+      await newUserChats.save();
+    } else {
+      // IF EXISTS, PUSH THE CHAT TO THE EXISTING ARRAY
+
+      await UserChats.updateOne(
+        { userId: userId },
+        {
+          $push: {
+            chats: {
+              _id: savedChat._id,
+              title: text.substring(0, 40),
+            },
+          },
+        }
+      );
+
+      res.status(201).send(newChat._id);
+    }
   } catch (error) {
     console.log(error);
     res.status(500).send("Error creating chat!");
   }
-
-  // try {
-  //   // CREATE A NEW CHAT
-  //   const newChat = new Chat({
-  //     userId: userId,
-  //     history: [{ role: "user", parts: [{ text }] }],
-  //   });
-
-  //   const savedChat = await newChat.save();
-
-  //   // CHECK IF THE USERCHATS EXISTS
-  //   const userChats = await UserChats.find({ userId: userId });
-
-  //   // IF DOESN'T EXIST CREATE A NEW ONE AND ADD THE CHAT IN THE CHATS ARRAY
-  //   if (!userChats.length) {
-  //     const newUserChats = new UserChats({
-  //       userId: userId,
-  //       chats: [
-  //         {
-  //           _id: savedChat._id,
-  //           title: text.substring(0, 40),
-  //         },
-  //       ],
-  //     });
-
-  //     await newUserChats.save();
-  //   } else {
-  //     // IF EXISTS, PUSH THE CHAT TO THE EXISTING ARRAY
-  //     await UserChats.updateOne(
-  //       { userId: userId },
-  //       {
-  //         $push: {
-  //           chats: {
-  //             _id: savedChat._id,
-  //             title: text.substring(0, 40),
-  //           },
-  //         },
-  //       }
-  //     );
-
-  //     res.status(201).send(newChat._id);
-  //   }
-  // } catch (err) {
-  //   console.log(err);
-  // }
 });
 
 app.listen(port, () => {
