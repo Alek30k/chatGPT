32dc8041bd76d84bc861717bb94f65ed5d86c870
Alek30k <alemank23k@gmail.com>
2024-09-25 15:12:29 -0300

    last commit

--- backend/index.js
+++ backend/index.js
@@ -35,6 +35,59 @@ app.get("/api/upload", (req, res) => {
   res.send(result);
 });
 
+app.post("/api/chats", async (req, res) => {
+  // const userId = req.auth.userId;
+  const { text } = req.body;
+
+  console.log(text);
+
+  // try {
+  //   // CREATE A NEW CHAT
+  //   const newChat = new Chat({
+  //     userId: userId,
+  //     history: [{ role: "user", parts: [{ text }] }],
+  //   });
+
+  //   const savedChat = await newChat.save();
+
+  //   // CHECK IF THE USERCHATS EXISTS
+  //   const userChats = await UserChats.find({ userId: userId });
+
+  //   // IF DOESN'T EXIST CREATE A NEW ONE AND ADD THE CHAT IN THE CHATS ARRAY
+  //   if (!userChats.length) {
+  //     const newUserChats = new UserChats({
+  //       userId: userId,
+  //       chats: [
+  //         {
+  //           _id: savedChat._id,
+  //           title: text.substring(0, 40),
+  //         },
+  //       ],
+  //     });
+
+  //     await newUserChats.save();
+  //   } else {
+  //     // IF EXISTS, PUSH THE CHAT TO THE EXISTING ARRAY
+  //     await UserChats.updateOne(
+  //       { userId: userId },
+  //       {
+  //         $push: {
+  //           chats: {
+  //             _id: savedChat._id,
+  //             title: text.substring(0, 40),
+  //           },
+  //         },
+  //       }
+  //     );
+
+  //     res.status(201).send(newChat._id);
+  //   }
+  // } catch (err) {
+  //   console.log(err);
+  //   res.status(500).send("Error creating chat!");
+  // }
+});
+
 app.listen(port, () => {
   connect();
   console.log("Server running on 3000");
