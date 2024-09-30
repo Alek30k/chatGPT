d7d8eeb269081aa01c4b24465fc5b53a10a80ba9
Alek30k <alemank23k@gmail.com>
2024-09-29 22:06:14 -0300

    last commit

--- client/src/routes/homePage/HomePage.jsx
+++ client/src/routes/homePage/HomePage.jsx
@@ -6,10 +6,6 @@ import { useState } from "react";
 const Homepage = () => {
   const [typingStatus, setTypingStatus] = useState("human1");
 
-  const test = async () => {
-    await fetch("http://localhost:3000/api/test", { credentials: "include" });
-  };
-
   return (
     <div className="homepage">
       <img src="/orbital.png" alt="" className="orbital" />
@@ -21,7 +17,6 @@ const Homepage = () => {
           dolorem doloribus, architecto dolor.
         </h3>
         <Link to="/dashboard">Get Started</Link>
-        <button onClick={test}>TEST BACKEND AUTH</button>
       </div>
       <div className="right">
         <div className="imgContainer">
