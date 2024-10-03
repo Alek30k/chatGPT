e712bb8e81069a2dcd36743beb9fdc53546e8b72
Alek30k <alemank23k@gmail.com>
2024-10-03 16:39:23 -0300

    last commit

--- client/src/components/chatList/ChatList.jsx
+++ client/src/components/chatList/ChatList.jsx
@@ -2,6 +2,14 @@ import { Link } from "react-router-dom";
 import "./chatList.css";
 
 const ChatList = () => {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["userChats"],
+    queryFn: () =>
+      fetch(`${import.meta.env.VITE_API_URL}/api/userchats`, {
+        credentials: "include",
+      }).then((res) => res.json()),
+  });
+
   return (
     <div className="chatList">
       <span className="title">DASHBOARD</span>
@@ -11,17 +19,15 @@ const ChatList = () => {
       <hr />
       <span className="title">RECENT CHATS</span>ChatList
       <div className="list">
-        <Link to="/">My chat title</Link>
-        <Link to="/">My chat title</Link>
-        <Link to="/">My chat title</Link>
-        <Link to="/">My chat title</Link>
-        <Link to="/">My chat title</Link>
-        <Link to="/">My chat title</Link>
-        <Link to="/">My chat title</Link>
-        <Link to="/">My chat title</Link>
-        <Link to="/">My chat title</Link>
-        <Link to="/">My chat title</Link>
-        <Link to="/">My chat title</Link>
+        {isPending
+          ? "Loading..."
+          : error
+          ? "Something went wrong!"
+          : data?.map((chat) => (
+              <Link to={`/dashboard/chats/${chat._id}`} key={chat._id}>
+                {chat.title}
+              </Link>
+            ))}
       </div>
       <hr />
       <div className="upgrade">
