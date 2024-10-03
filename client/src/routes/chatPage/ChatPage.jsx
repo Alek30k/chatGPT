f69357da92e2a9ebe8c9ad854a96a92239a2716e
Alek30k <alemank23k@gmail.com>
2024-10-03 16:56:50 -0300

    last commit

--- client/src/routes/chatPage/ChatPage.jsx
+++ client/src/routes/chatPage/ChatPage.jsx
@@ -1,53 +1,26 @@
+import { useLocation } from "react-router-dom";
 import NewPrompt from "../../components/newPrompt/NewPrompt";
 import "./chatPage.css";
+import { useQuery } from "@tanstack/react-query";
 
 const ChatPage = () => {
+  const path = useLocation().pathname;
+  const chatId = path.split("/").pop();
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ["chat", chatId],
+    queryFn: () =>
+      fetch(`${import.meta.env.VITE_API_URL}/api/chats/${chatId}`, {
+        credentials: "include",
+      }).then((res) => res.json()),
+  });
+
+  console.log(data);
+
   return (
     <div className="chatPage">
       <div className="wrapper">
         <div className="chat">
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>{" "}
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>{" "}
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>{" "}
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>{" "}
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>{" "}
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>{" "}
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>{" "}
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>{" "}
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>{" "}
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>
           <div className="message">message from ia</div>
           <div className="message user">message from user</div>
           <NewPrompt />
