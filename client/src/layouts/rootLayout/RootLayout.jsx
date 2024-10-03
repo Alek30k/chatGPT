d6aeff3221a9709db8498f405d713c4605320d4a
Alek30k <alemank23k@gmail.com>
2024-10-03 15:43:59 -0300

    last commit

--- client/src/layouts/rootLayout/RootLayout.jsx
+++ client/src/layouts/rootLayout/RootLayout.jsx
@@ -1,40 +1,40 @@
 import { Link, Outlet } from "react-router-dom";
 import "./rooLayout.css";
-import {
-  ClerkProvider,
-  SignedIn,
-  // SignedOut,
-  // SignInButton,
-  UserButton,
-} from "@clerk/clerk-react";
+import { ClerkProvider, SignedIn, UserButton } from "@clerk/clerk-react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
+
+const queryClient = new QueryClient();
+
 const RootLayout = () => {
   return (
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <div className="rootLayout">
-        <header>
-          <Link to="/" className="logo">
-            <img src="/logo.png" alt="logo" />
-            <span>ALE IA</span>
-          </Link>
-          <div className="user">
-            {/* <SignedOut>
+      <QueryClientProvider client={queryClient}>
+        <div className="rootLayout">
+          <header>
+            <Link to="/" className="logo">
+              <img src="/logo.png" alt="logo" />
+              <span>ALE IA</span>
+            </Link>
+            <div className="user">
+              {/* <SignedOut>
               <SignInButton />
             </SignedOut> */}
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </div>
-        </header>
-        <main>
-          <Outlet />
-        </main>
-      </div>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </header>
+          <main>
+            <Outlet />
+          </main>
+        </div>
+      </QueryClientProvider>
     </ClerkProvider>
   );
 };
