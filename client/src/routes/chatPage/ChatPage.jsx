05b469560c496a5269c442d1486346abff0c41c9
Alek30k <alemank23k@gmail.com>
2024-10-03 22:56:55 -0300

    last commit

--- client/src/routes/chatPage/ChatPage.jsx
+++ client/src/routes/chatPage/ChatPage.jsx
@@ -2,6 +2,7 @@ import { useLocation } from "react-router-dom";
 import NewPrompt from "../../components/newPrompt/NewPrompt";
 import "./chatPage.css";
 import { useQuery } from "@tanstack/react-query";
+import Markdown from "react-markdown";
 
 const ChatPage = () => {
   const path = useLocation().pathname;
@@ -21,8 +22,33 @@ const ChatPage = () => {
     <div className="chatPage">
       <div className="wrapper">
         <div className="chat">
-          <div className="message">message from ia</div>
-          <div className="message user">message from user</div>
+          {isPending
+            ? "Loading..."
+            : error
+            ? "Something went wrong!"
+            : data?.history?.map((message, i) => (
+                <>
+                  {message.img && (
+                    <IKImage
+                      urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+                      path={message.img}
+                      height="300"
+                      width="400"
+                      transformation={[{ height: 300, width: 400 }]}
+                      loading="lazy"
+                      lqip={{ active: true, quality: 20 }}
+                    />
+                  )}
+                  <div
+                    className={
+                      message.role === "user" ? "message user" : "message"
+                    }
+                    key={i}
+                  >
+                    <Markdown>{message.parts[0].text}</Markdown>
+                  </div>
+                </>
+              ))}
           <NewPrompt />
         </div>
       </div>
