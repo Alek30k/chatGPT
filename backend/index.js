8c2559b0e152565935bd6b83d4753ddbdd96da73
Alek30k <alemank23k@gmail.com>
2024-10-16 17:30:14 -0300

    last commit

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import path from "path";
 // import url, { fileURLToPath } from "url";
 import ImageKit from "imagekit";
 import mongoose from "mongoose";
